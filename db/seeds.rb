25c9c77bad1c2382556e8e6815cb42c86c1b785b
Snehanjali Kalamkar <ksnehanjali@gmail.com>
2020-01-22 16:49:35 +0530

    Added cities and sports to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cities = City.create([
+          {name: 'Pune'},
+          {name: 'Mumbai'},
+          {name: 'New Delhi'},
+          {name: 'Chennai'},
+          {name: 'Chandigarh'},
+          {name: 'Kolkata'},
+          {name: 'Gurgaon'},
+          {name: 'Noida'},
+          {name: 'Jaipur'},
+          {name: 'Bengaluru'},
+          {name: 'Navi Mumbai'},
+          {name: 'Hyderabad'},
+          {name: 'Ahmedabad'},
+          {name: 'Goa'},
+          {name: 'Nagpur'},
+          {name: 'Patna'},
+          {name: 'Lucknow'},
+          {name: 'Aizawl'}
+        ])
+
+sports = Sport.create([
+          {name: 'Badminton'},
+          {name: 'Table Tennis'},
+          {name: 'Squash'},
+          {name: 'Tennis'},
+          {name: 'Racquetball'}
+        ])
