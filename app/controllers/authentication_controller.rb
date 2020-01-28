5fe0d28050ded151215570147878f3d8d65a99f8
DevKaranPradhan <dvpradhan312@gmail.com>
2020-01-28 10:22:38 +0530

    sign in

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,6 @@
 class AuthenticationController < ApplicationController
   def login
-    # @user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_and_belongs_to_many :matches
   has_many :user_city_sports
 end
