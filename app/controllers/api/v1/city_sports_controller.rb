3c3ffae794322ea92a39b20853b6e02a93cbe6a8
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-03 21:14:50 +0530

    Added sports list for city

--- app/controllers/api/v1/city_sports_controller.rb
+++ app/controllers/api/v1/city_sports_controller.rb
@@ -1,3 +1,7 @@
+#require 'lib/json_web_token.rb'
+#SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
+require 'jwt'
+$hmac_secret = "your-256-bit-secret"
 module Api
     module V1
         class CitySportsController < ApplicationController
@@ -13,10 +17,11 @@ module Api
                 render json: @sport_list.as_json
             end
 
-
-            def displaysports
-                @user_id = params['user_id']
-                @user = User.where(id: 1)
+            def displaysports    
+                @token_array = JWT.decode(params['token'],$hmac_secret,true,{algorithm: 'HS256'})
+                @token = @token_array[0]
+                @user_id = @token["user_id"]
+                @user = User.where(id: @user_id)
                 @city_id = @user.first.city_id
                 @sport_list = []
                 @city_sports = CitySport.where(city_id: @city_id)
@@ -24,14 +29,11 @@ module Api
                     @sport_list = @sport_list + Sport.where(id: citysport.sport_id)
                 end    
                 render json: @sport_list.as_json
-                
             end
 
             def permit_params
                 params.permit(:user).permit(:user_id)
             end
-
-
         end 
     end
 end
