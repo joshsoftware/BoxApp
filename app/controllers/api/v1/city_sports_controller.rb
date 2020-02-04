c266abd1dc19ab51d5883192982faf0bce02bfae
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-04 10:09:10 +0530

    removed index method

--- app/controllers/api/v1/city_sports_controller.rb
+++ app/controllers/api/v1/city_sports_controller.rb
@@ -3,37 +3,25 @@
 require 'jwt'
 $hmac_secret = "your-256-bit-secret"
 module Api
-    module V1
-        class CitySportsController < ApplicationController
-            def index
-                #@user_id = params['user_id']
-                @user = User.where(id: 1)
-                @city_id = @user.first.city_id
-                @sport_list = []
-                @city_sports = CitySport.where(city_id: @city_id)
-                @city_sports.find_each do |citysport| 
-                    @sport_list = @sport_list + Sport.where(id: citysport.sport_id)
-                end    
-                render json: @sport_list.as_json
-            end
+  module V1
+    class CitySportsController < ApplicationController
+      def displaysports    
+        @token_array = JWT.decode(params['token'],$hmac_secret,true,{algorithm: 'HS256'})
+        @token = @token_array[0]
+        @user_id = @token["user_id"]
+        @user = User.where(id: @user_id)
+        @city_id = @user.first.city_id
+        @sport_list = []
+        @city_sports = CitySport.where(city_id: @city_id)
+        @city_sports.find_each do |citysport| 
+          @sport_list = @sport_list + Sport.where(id: citysport.sport_id)
+        end    
+        render json: @sport_list.as_json
+      end
 
-            def displaysports    
-                @token_array = JWT.decode(params['token'],$hmac_secret,true,{algorithm: 'HS256'})
-                @token = @token_array[0]
-                @user_id = @token["user_id"]
-                @user = User.where(id: @user_id)
-                @city_id = @user.first.city_id
-                @sport_list = []
-                @city_sports = CitySport.where(city_id: @city_id)
-                @city_sports.find_each do |citysport| 
-                    @sport_list = @sport_list + Sport.where(id: citysport.sport_id)
-                end    
-                render json: @sport_list.as_json
-            end
-
-            def permit_params
-                params.permit(:user).permit(:user_id)
-            end
-        end 
-    end
+      def permit_params
+        params.permit(:user).permit(:user_id)
+      end
+    end 
+  end
 end
