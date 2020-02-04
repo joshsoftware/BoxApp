42fa090694e5bd5ae7f29f449a26a2e0880a8af8
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-04 17:25:05 +0530

    Redirect to set password

--- app/controllers/api/v1/mailers_controller.rb
+++ app/controllers/api/v1/mailers_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MailersController < ActionController::Base
       def confirm_link
-        return redirect_to "http://www.google.com"
+        return redirect_to "app://boxapp/setpassword/confirmation?confirmation_token=<%= @token %>"
         # render json: { data: render_to_string('confirm_link.html.erb') }
       end
     end
