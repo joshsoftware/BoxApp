75ad997d6c6f29775ab4d2fbde42851951b040ea
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-05 10:32:47 +0530

    added factory bot

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,9 +2,10 @@ require 'faker'
 Faker::Config.random = Random.new(42)
 
 FactoryBot.define do
-    factory :new_user do
+    factory :user do
         name { Faker::Name.name }
         contact_number { Faker::Number.number(digits: 4)}
         role { Faker::Boolean.boolean }
+        city_id { Faker::Number.between(from: 1, to:2)}
     end
 end 
