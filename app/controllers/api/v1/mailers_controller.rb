17e633787032e196a9ec41a896a0eadce65b93f3
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-05 17:33:32 +0530

    Updated redirection logic

--- app/controllers/api/v1/mailers_controller.rb
+++ app/controllers/api/v1/mailers_controller.rb
@@ -3,7 +3,12 @@ module Api
     class MailersController < ApplicationController
       def confirm_link
         @token = request.params['confirmation_token']
-        return redirect_to "app://boxapp/setpassword?confirm=#{@token}"
+        @user = User.find_by(confirmation_token: @token)
+        if @user
+          return redirect_to "app://boxapp/setpassword?confirm=#{@token}"
+        else
+          return redirect_to "app://boxapp/intro"
+        end
       end
     end
   end
