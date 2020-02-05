e2c41139e16ce5695c68ab58db3811666ce702d1
25shwetak <25shwetakshasha.com>
2020-02-05 17:07:00 +0530

    api for boxes

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -1,12 +1,12 @@
 module Api
-    module V1
-      class LevelsController < ApplicationController
+  module V1
+    class LevelsController < ApplicationController
   
-        def index
-          render json: Level.all.as_json
-        end
-  
+      def index
+        render json: Level.all.as_json
       end
+  
     end
   end
+end
   
