ef2372b43b68d0d8fbc1a4b16fbaec00726895e5
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-07 17:09:09 +0530

    removed token validation

--- app/controllers/api/v1/city_sports_controller.rb
+++ app/controllers/api/v1/city_sports_controller.rb
@@ -1,17 +1,15 @@
 module Api
   module V1
-    class Api::V1::CitySportsController < ApplicationController
-      def displaysports    
-        @token = JsonWebToken.decode(params['token'])
-        @user_id = @token["user_id"]
-        @user = User.where(id: @user_id)
-        @city_id = @user.first.city_id
-        @sport_list = []
-        @city_sports = CitySport.where(city_id: @city_id)
-        @city_sports.find_each do |citysport| 
-          @sport_list = @sport_list + Sport.where(id: citysport.sport_id)
+    class CitySportsController < ApplicationController
+
+      def displaysports   
+        city_id = @current_user.city_id
+        sport_list = []
+        city_sports = CitySport.where(city_id: city_id)
+        city_sports.find_each do |citysport| 
+          sport_list = sport_list + Sport.where(id: citysport.sport_id)
         end    
-        render json: @sport_list.as_json
+        render json: sport_list.as_json
       end
     end 
   end
