af59a4e291ad363bcc15690e0ca2200b17f71350
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-07 23:17:25 +0530

    added test case for unauthorized user

--- spec/requests/post_opponents_listing_spec.rb
+++ spec/requests/post_opponents_listing_spec.rb
@@ -11,11 +11,15 @@ describe "post token and return opponents list", :type => :request do
     LevelCitySport.create(city_sport_id: 1,level_id: 1)
     UserCitySport.create(user_id: 1,city_sport_id: 1,level_city_sports_id:1)
     UserCitySport.create(user_id: 2 ,city_sport_id: 1,level_city_sports_id:1)
-    post '/api/v1/list_opponents', params: {:token => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"} 
+    get '/api/v1/list_opponents', headers: {"user-auth-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"} 
   end
 
   it 'returns a ok status' do
     expect(response).to have_http_status(:ok)
   end
+
+  it 'returns unauthorized error' do
+    expect(response). to have_http_status(:unauthorized)
+  end 
 
 end
