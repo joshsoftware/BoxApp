e829fa629cf89bc916806637d6d421981a6d5b58
25shwetak <25shwetakshasha.com>
2020-02-07 13:30:50 +0530

    Fixed offenses

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Level < ApplicationRecord
-  validates :name, :description, presence: true  
-  validates :name, uniqueness: true   
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
   has_many :level_city_sports
 end
--- spec/requests/get_level_spec.rb
+++ spec/requests/get_level_spec.rb
@@ -1,12 +1,13 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-describe "get all levels", :type => :request do
-    
-    before do
-      get '/api/v1/levels'
-    end
+require "rails_helper"
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
-end  
+describe "get all levels", type: :request do
+  before do
+    get "/api/v1/levels"
+  end
+
+  it "returns http success" do
+    expect(response).to have_http_status(:success)
+  end
+end
