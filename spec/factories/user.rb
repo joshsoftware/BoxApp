607a4e53390d191ad1beb04fdd1ec5d1bf5a4438
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-09 01:44:54 +0530

    Updated factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,19 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    Faker::Config.locale = "en-IND"
-    email    { Faker::Internet.email }
-    password {"123456"}
-    password_cofirmation {"123456"}
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    is_admin {false}
-    city_id {1}
-    contact_number {"1230234983"}#{ Faker::PhoneNumber.phone_number }
-    # email {'temp@gmail.com'}
-    # password {'temp123'}
-    # first_name {'temp'}
-    # last_name {'kumar'}
-    # contact_number {'1230984567'}
-
+    email { Faker::Internet.email }
+    contact_number { Faker::Number.number(digits: 10) }
+    is_admin { Faker::Boolean.boolean }
+    city_id { Faker::Number.between(from: 1, to: 1) }
   end
 end
