c28f725b4c2e51da12b249adca174298159d157f
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-09 01:47:15 +0530

    Updated testcases

--- spec/requests/sign_in_spec.rb
+++ spec/requests/sign_in_spec.rb
@@ -1,20 +1,28 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-describe "sign in", :type => :request do
+require "rails_helper"
 
-    before do
-      Sport.create(name: "Badminton")
-      City.create(name: "Pune")
-      CitySport.create(city_id: 1, sport_id: 1, number_of_boxes: 5,number_of_players: 10)
-      Level.create(name: "B1")
-      User.create(first_name: "mayur",last_name: "deshmukh",contact_number: "2132131231", is_admin: false, email: "ksnehanjali@gmail.com",city_id: 1, password: "123456", password_confirmation: "123456")
-      post  '/api/v1/sessions', 
-            params: {
-                      "email":"ksnehanjali@gmail.com",
-                      "password":"123456"
-                    }
-    end
-    it 'returns an ok status' do
-      expect(response).to have_http_status(:ok)
-    end
-end                 
+describe "sign in", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    @user.password = "123456"
+    @user.password_confirmation = "123456"
+    @user.save
+  end
+  it "returns an ok status" do
+    post  "/api/v1/sessions",
+          params: {
+            "email":    @user.email,
+            "password": @user.password
+          }
+    expect(response).to have_http_status(:ok)
+  end
+  it "returns unauthorized status" do
+    post  "/api/v1/sessions",
+          params: {
+            "email":    @user.email,
+            "password": "1234567"
+          }
+    expect(response).to have_http_status(:unauthorized)
+  end
+end
