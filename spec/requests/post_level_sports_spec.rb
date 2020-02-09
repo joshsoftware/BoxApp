3b8475a45ce65ffa4eca1e83d5bb0dd44d1de024
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-09 11:57:32 +0530

    added test cases for token validation

--- spec/requests/post_level_sports_spec.rb
+++ spec/requests/post_level_sports_spec.rb
@@ -4,15 +4,35 @@ describe "post a level and sport", :type => :request do
     
     before do
         FactoryBot.create(:user)
-        Sport.create(name: "Badminton")
-        City.create(name: "Pune")
-        CitySport.create(city_id: 1, sport_id: 1, number_of_boxes: 5,number_of_players: 10)
-        Level.create(name: "B1")
+        sport = Sport.create(name: "Badminton")
+        city = City.create(name: "Pune")
+        CitySport.create(city_id: city.id, sport_id: sport.id, number_of_boxes: 5,number_of_players: 10)
+        level = Level.create(name: "B1")
 
-        post '/api/v1/level_sports', params: {:token => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY",:sport_id => "1",:level_name => "B1"}
+        post '/api/v1/level_sports', headers: {'user-auth-token' => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"} ,params: {:sport_id => sport.id,:level_id => level.id}
     end
 
     it 'returns a ok status' do
         expect(response).to have_http_status(:ok)
     end
-end                 
+end
+
+describe "should return unauthorized due to user not present", :type => :request do
+    before do
+        post '/api/v1/level_sports',  headers: {'user-auth-token' => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"} ,params: {:sport_id => 1,:level_id => 1}   
+    end
+
+    it 'return unauthorized status' do
+        expect(response).to have_http_status(:unauthorized)
+    end
+end
+
+describe "should return unauthorized due invalid jwt token", :type => :request do
+    before do
+        post '/api/v1/level_sports',  headers: {'user-auth-token' => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5Y"} ,params: {:sport_id => 1,:level_id => 1}   
+    end
+
+    it 'return unauthorized status' do
+        expect(response).to have_http_status(:unauthorized)
+    end
+end
