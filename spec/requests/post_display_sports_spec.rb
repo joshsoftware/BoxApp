d638a79721ac270d05f047d0acb35f8ae3dd171c
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-09 12:10:57 +0530

    added test cases for token validation

--- spec/requests/post_display_sports_spec.rb
+++ spec/requests/post_display_sports_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "post a token and display sports", :type => :request do
+describe "should display sports", :type => :request do
 
     before do
         FactoryBot.create(:user)
@@ -8,9 +8,35 @@ describe "post a token and display sports", :type => :request do
         City.create(name: "Pune")
         CitySport.create(city_id: 1, sport_id: 1, number_of_boxes: 5,number_of_players: 10)
 
-        post '/api/v1/city_sports/display', params: {:token => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"}
+        get '/api/v1/city_sports/display', headers: {"user-auth-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"} 
     end
     it 'returns a ok status' do
         expect(response).to have_http_status(:ok)
     end
 end
+
+describe "should return unauthorized due to user not present", :type => :request do
+
+    before do
+        FactoryBot.create(:user)
+        get '/api/v1/city_sports/display', headers: {"user-auth-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOCJ9.9V492uKokeLSu9F62ck-CX2XvrquAsTNc9_agkPcYzk"}
+    end
+
+    it 'return a unauthorized status' do
+        expect(response).to have_http_status(:unauthorized)
+    end
+end
+
+describe "should return unauthorized due to invalid jwt token", :type => :request do
+
+    before do
+        FactoryBot.create(:user)
+        get '/api/v1/city_sports/display', headers: {"user-auth-token" => "eyJhbGciOiJIUzI1NiIsInR5cuKokeLSu9F62ck-CX2XvrquAsTNc9_agkPcYzk"}
+    end
+
+    it 'return a unauthorized status' do
+        expect(response).to have_http_status(:unauthorized)
+    end
+end
+
+        
