90b15ef0051b36cfce55a5010811f507d2a542aa
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-09 11:57:06 +0530

    added token validation method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  before_action :validate_token
+  
+  def validate_token
+    begin
+      user_auth_token = request.headers["user-auth-token"]
+      @current_user = current_user(user_auth_token)
+
+    rescue JWT::DecodeError => e
+      render json: {error: e.message}, status: :unauthorized
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {error: "Unauthorized"}, status: :unauthorized
+    end
+  end
+  
+  def current_user(user_auth_token)
+    token = JsonWebToken.decode(user_auth_token)
+    user_id = token["user_id"]
+    user = User.find(user_id)
+    return user
+  end
 end
