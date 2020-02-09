fd6567485f991dd51d11eb449410f219a52fc745
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-09 17:28:45 +0530

    Resolved offenses

--- spec/requests/post_display_sports_spec.rb
+++ spec/requests/post_display_sports_spec.rb
@@ -10,6 +10,7 @@ describe "should display sports", type: :request do
     CitySport.create(city_id: 1, sport_id: 1, number_of_boxes: 5, number_of_players: 10)
     @token1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"
     @token2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOCJ9.9V492uKokeLSu9F62ck-CX2XvrquAsTNc9_agkPcYzk"
+    @token3 = "eyJhbGciOiJIUzI1NiIsInR5cuKokeLSu9F62ck-CX2XvrquAsTNc9_agkPcYzk"
     get "/api/v1/city_sports/display",
         headers: {"user-auth-token" => @token1}
   end
@@ -34,10 +35,7 @@ describe "should return unauthorized due to invalid jwt token", type: :request d
   before do
     FactoryBot.create(:user)
     get "/api/v1/city_sports/display",
-        headers: {
-          "user-auth-token" =>
-                               "eyJhbGciOiJIUzI1NiIsInR5cuKokeLSu9F62ck-CX2XvrquAsTNc9_agkPcYzk"
-        }
+        headers: {"user-auth-token" => @token3}
   end
 
   it "return a unauthorized status" do
