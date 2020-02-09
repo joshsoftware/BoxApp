ae085e93c698910d8072f4a71923c0345c6b32e2
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-09 11:56:42 +0530

    removed instance variables and token decoding

--- app/controllers/api/v1/level_sports_controller.rb
+++ app/controllers/api/v1/level_sports_controller.rb
@@ -1,16 +1,13 @@
 module Api
   module V1
-    class Api::V1::LevelSportsController < ApplicationController
-      def create
-        @token = JsonWebToken.decode(params['token'])
-        @user_id = @token["user_id"]
-        @user = User.where(id: @user_id)
-        @city_id = @user.first.city_id
-        @sport_id = params["sport_id"]
-        @city_sport_id = CitySport.find_by(city_id: @city_id, sport_id: @sport_id)
-        @level_id = params["level_id"]
-        @level_city_sports = LevelCitySport.where(city_sport_id: @city_sport_id.id, level_id: @level_id)
-        UserCitySport.create(user_id: @user_id, city_sport_id: @city_sport_id.id,level_city_sports_id: @level_city_sports.ids[0])
+    class LevelSportsController < ApplicationController
+      def create 
+        city_id = @current_user.city_id
+        sport_id = params["sport_id"]
+        city_sport_id = CitySport.find_by(city_id: city_id, sport_id: sport_id)
+        level_id = params["level_id"]
+        level_city_sports = LevelCitySport.where(city_sport_id: city_sport_id.id, level_id: level_id)
+        UserCitySport.create(user_id: @current_user.id, city_sport_id: city_sport_id.id,level_city_sports_id: level_city_sports.ids[0])
         render json: {status: "ok"}
       end 
     end
