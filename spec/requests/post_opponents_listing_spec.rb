8bafbf9b6e9f25d8e127b5c26b5f8951f6f6f13b
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-10 12:17:44 +0530

    Resolved offenses

--- spec/requests/post_opponents_listing_spec.rb
+++ spec/requests/post_opponents_listing_spec.rb
@@ -1,66 +1,95 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-describe "should return opponents count ", :type => :request do
-  before do 
-    FactoryBot.create(:user)
-    FactoryBot.create(:user)
-    Sport.create(name: "Badminton")
-    City.create(name: "Pune")
-    CitySport.create(city_id: 1,sport_id: 1,number_of_boxes: 5,number_of_players: 5)
-    Level.create(name: "B1")
-    LevelCitySport.create(city_sport_id: 1,level_id: 1)
-    UserCitySport.create(user_id: 1,city_sport_id: 1,level_city_sports_id:1)
-    UserCitySport.create(user_id: 2 ,city_sport_id: 1,level_city_sports_id:1)
-    get '/api/v1/list_opponents', headers: {"user-auth-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"} 
+require "rails_helper"
+
+describe "should return opponents count ", type: :request do
+  before do
+    @sport1 = Sport.create(name: "Badminton")
+    @city1 = City.create(name: "Pune")
+    @user1 = FactoryBot.create(:user, city_id: @city1.id)
+    @user2 = FactoryBot.create(:user, city_id: @city1.id)
+    @citysport1 = CitySport.create(city_id:           @city1.id,
+                                   sport_id:          @sport1.id,
+                                   number_of_boxes:   5,
+                                   number_of_players: 5)
+    @level1 = Level.create(name: "B1")
+    @levelcitysport1 = LevelCitySport.create(city_sport_id: @citysport1.id,
+                                             level_id:      @level1.id)
+    @usercitysport1 = UserCitySport.create(user_id:              @user1.id,
+                                           city_sport_id:        @citysport1.id,
+                                           level_city_sports_id: @levelcitysport1.id)
+    @usercitysport2 = UserCitySport.create(user_id:              @user2.id,
+                                           city_sport_id:        @citysport1.id,
+                                           level_city_sports_id: @levelcitysport1.id)
+    @token1 = JsonWebToken.encode(user_id: @user1.id)
   end
 
-  it 'returns a ok status' do
+  it "returns a ok status" do
+    get "/api/v1/list_opponents", headers: {"user-auth-token" => @token1}
     expect(response).to have_http_status(:ok)
   end
 
-  it 'returns unauthorized error due to user_id not present' do
-    expect(response). to have_http_status(:unauthorized)
-  end 
-end
-
-describe "should return opponents list", :type => :request do
-  before do 
-    FactoryBot.create(:user)
-    FactoryBot.create(:user)
-    FactoryBot.create(:user)
-    FactoryBot.create(:user)
-    FactoryBot.create(:user)
-    FactoryBot.create(:user)
-    FactoryBot.create(:user)
-    Sport.create(name: "Badminton")
-    City.create(name: "Pune")
-    CitySport.create(city_id: 1,sport_id: 1,number_of_boxes: 5,number_of_players: 5)
-    Level.create(name: "B1")
-    LevelCitySport.create(city_sport_id: 1,level_id: 1)
-    UserCitySport.create(user_id: 3,city_sport_id: 1,level_city_sports_id:1)
-    UserCitySport.create(user_id: 4 ,city_sport_id: 1,level_city_sports_id:1)
-    UserCitySport.create(user_id: 5 ,city_sport_id: 1,level_city_sports_id:1)
-    UserCitySport.create(user_id: 6 ,city_sport_id: 1,level_city_sports_id:1)
-    UserCitySport.create(user_id: 7 ,city_sport_id: 1,level_city_sports_id:1)
-    UserCitySport.create(user_id: 8 ,city_sport_id: 1,level_city_sports_id:1)
-    UserCitySport.create(user_id: 9 ,city_sport_id: 1,level_city_sports_id:1)
-    get '/api/v1/list_opponents', headers: {"user-auth-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMyJ9.IpKHz7JL6PXBjYOAPZ4BUBUZHUdf7OwMKGJkPs-ixQA"} 
-  end
-
-  it 'returns a ok status' do
-    expect(response).to have_http_status(:ok)
-  end
-end
-
-describe "should return error due jwt invalidation", :type => :request do
-  before do 
-    get '/api/v1/list_opponents', headers: {"user-auth-token" => "eyJhbGciOiJIRVyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"} 
-  end
-
-  it 'returns unauthorized error due to JWT token error' do
+  it "returns unauthorized error due to user_id not present" do
+    get "/api/v1/list_opponents", headers: {"user-auth-token" => JsonWebToken.encode(user_id: 123)}
     expect(response). to have_http_status(:unauthorized)
   end
 end
 
+describe "should return opponents list", type: :request do
+  before do
+    @sport2 = Sport.create(name: "Badminton")
+    @city2 = City.create(name: "Pune")
+    @user3 = FactoryBot.create(:user, city_id: @city2.id)
+    @user4 = FactoryBot.create(:user, city_id: @city2.id)
+    @user5 = FactoryBot.create(:user, city_id: @city2.id)
+    @user6 = FactoryBot.create(:user, city_id: @city2.id)
+    @user7 = FactoryBot.create(:user, city_id: @city2.id)
+    @user8 = FactoryBot.create(:user, city_id: @city2.id)
+    @user9 = FactoryBot.create(:user, city_id: @city2.id)
 
+    @citysport2 = CitySport.create(city_id:           @city2.id,
+                                   sport_id:          @sport2.id,
+                                   number_of_boxes:   5,
+                                   number_of_players: 5)
+    @level2 = Level.create(name: "B1")
+    @levelcitysport2 = LevelCitySport.create(city_sport_id: @citysport2.id, level_id: @level2.id)
+    @usercitysport3 = UserCitySport.create(user_id:              @user3.id,
+                                           city_sport_id:        @citysport2.id,
+                                           level_city_sports_id: @levelcitysport2.id)
+    @usercitysport4 = UserCitySport.create(user_id:              @user4.id,
+                                           city_sport_id:        @citysport2.id,
+                                           level_city_sports_id: @levelcitysport2.id)
+    @usercitysport5 = UserCitySport.create(user_id:              @user5.id,
+                                           city_sport_id:        @citysport2.id,
+                                           level_city_sports_id: @levelcitysport2.id)
+    @usercitysport6 = UserCitySport.create(user_id:              @user6.id,
+                                           city_sport_id:        @citysport2.id,
+                                           level_city_sports_id: @levelcitysport2.id)
+    @usercitysport7 = UserCitySport.create(user_id:              @user7.id,
+                                           city_sport_id:        @citysport2.id,
+                                           level_city_sports_id: @levelcitysport2.id)
+    @usercitysport8 = UserCitySport.create(user_id:              @user8.id,
+                                           city_sport_id:        @citysport2.id,
+                                           level_city_sports_id: @levelcitysport2.id)
+    @usercitysport9 = UserCitySport.create(user_id:              @user9.id,
+                                           city_sport_id:        @citysport2.id,
+                                           level_city_sports_id: @levelcitysport2.id)
+    @token2 = JsonWebToken.encode(user_id: @user3.id)
+    get "/api/v1/list_opponents", headers: {"user-auth-token" => @token2}
+  end
 
+  it "returns a ok status" do
+    expect(response).to have_http_status(:ok)
+  end
+end
+
+describe "should return error due jwt invalidation", type: :request do
+  before do
+    @token3 = "eyJhbGciOiJIRVyX2lkIjoiMSJ9.O5Y5s_hvEW8BM7E8jq6HihxQ0DDFxO_2_xtnrvVj4PY"
+    get "/api/v1/list_opponents", headers: {"user-auth-token" => @token3}
+  end
+
+  it "returns unauthorized error due to JWT token error" do
+    expect(response). to have_http_status(:unauthorized)
+  end
+end
