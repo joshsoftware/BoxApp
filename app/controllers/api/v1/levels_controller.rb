3d20a0db707719edb3209c26a2e522aa473544f7
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-10 15:09:23 +0530

    Api to fetch levels for a city-sport

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -3,8 +3,26 @@
 module Api
   module V1
     class LevelsController < ApplicationController
+      skip_before_action :validate_token, only: [:index]
       def index
         render json: Level.all.as_json
+      end
+
+      def all_levels
+        city_id = @current_user.city_id
+        sport_id = params["sport_id"]
+        city_sport = CitySport.find_by(city_id: city_id, sport_id: sport_id)
+        level_list = []
+        level_city_sports = LevelCitySport.where(city_sport_id: city_sport.id)
+        level_city_sports.find_each do |level|
+          level_obj = Level.find_by(id: level.level_id)
+          number_of_players = UserCitySport.where(level_city_sports_id: level.id).count
+          level_hash = {"name"        => level_obj.name,
+                        "description" => level_obj.description,
+                        "count"       => city_sport.number_of_players - number_of_players}
+          level_list.append(level_hash)
+        end
+        render json: level_list.as_json
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post "/city_sport_levels", to: "levels#all_levels"
       resources :levels
     end
   end
