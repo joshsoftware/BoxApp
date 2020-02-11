215a9938c3504373a69c2fd8cc6f8d8cb6665c74
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-11 22:37:26 +0530

    Updated query

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -12,7 +12,14 @@ module Api
         city_id = @current_user.city_id
         sport_id = params["sport_id"]
         city_sport = CitySport.find_by(city_id: city_id, sport_id: sport_id)
-        level_list = LevelCitySport.select("levels.id as id, levels.name as name, levels.description as description, #{city_sport.number_of_players} - (SELECT COUNT(*) FROM user_city_sports WHERE user_city_sports.level_city_sports_id = level_city_sports.id) as free_slots").joins(:city_sport, :level, "LEFT JOIN user_city_sports ON user_city_sports.level_city_sports_id = level_city_sports.id").where(city_sport_id: 1).uniq
+        slots_query = "#{city_sport.number_of_players} - (SELECT COUNT(*) FROM user_city_sports WHERE ",
+                      "user_city_sports.level_city_sports_id = level_city_sports.id) as free_slots"
+        attribute = "levels.id as id",
+                    "levels.name as name",
+                    "levels.description as description",
+                    slots_query[0] + slots_query[1]
+        level_list = LevelCitySport.select(attribute[0], attribute[1], attribute[2], attribute[3])
+                                   .joins(:level).where(city_sport_id: city_sport.id).uniq
         render json: level_list.as_json
       end
     end
