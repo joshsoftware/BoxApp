eeb0580a89b41d0a99fe848c005a128814d7e49f
niharikabhoi2020 <niharikabhoi2016@gmail.com>
2020-02-11 16:27:52 +0530

    Resolved merge conflicts with user_location

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+
 module Api
   module V1
     class CitiesController < ApplicationController
-      skip_before_action :validate_token, only: %i[index create]
       def index
-        render json: City.all.as_json
+        render json: City.all.order(:name).as_json
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,12 @@ Rails.application.routes.draw do
       post "/level_sports", to: "level_sports#create"
       post "/city_sport_levels", to: "levels#all_levels"
       resources :levels
-      resources :cities
+      # resources :cities
       get "/city_sports/display", to: "city_sports#displaysports"
       resources :city_sports
       get "/list_opponents", to: "list_opponents#opponents"
       # resources :confirmations, only: [:update, :show]
+      resources :cities, only: [:index]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
