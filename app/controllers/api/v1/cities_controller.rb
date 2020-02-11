63dc7976bf2c22c977f47db61cacc417bd894111
25shwetak <25shwetakshasha.com>
2020-02-11 17:24:10 +0530

    Removed commented portion from cities_controller.rb

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-# class CitiesController < ActionController::API
-#     def index
-#         render json: City.all.as_json
-#     end
-# end
-
 module Api
   module V1
     class CitiesController < ApplicationController
