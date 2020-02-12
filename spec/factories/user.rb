4ae9f2d453dcabb7edf4ab0bf8d021ec9f578011
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-12 11:18:47 +0530

    added factories for city,sport,level,user

---
+++ spec/factories/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    contact_number { Faker::Number.number(digits: 4) }
+    role { Faker::Boolean.boolean }
+    city_id { Faker::Number.between(from: 1, to: 1) }
+  end
+end
