624795a1833e1e2d5a9a227383c72198111dfd89
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-12 13:01:46 +0530

    Added error handling

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -12,15 +12,20 @@ module Api
         city_id = @current_user.city_id
         sport_id = params["sport_id"]
         city_sport = CitySport.find_by(city_id: city_id, sport_id: sport_id)
-        slots_query = "#{city_sport.number_of_players} - (SELECT COUNT(*) FROM user_city_sports WHERE ",
-                      "user_city_sports.level_city_sports_id = level_city_sports.id) as free_slots"
-        attribute = "levels.id as id",
-                    "levels.name as name",
-                    "levels.description as description",
-                    slots_query[0] + slots_query[1]
-        level_list = LevelCitySport.select(attribute[0], attribute[1], attribute[2], attribute[3])
-                                   .joins(:level).where(city_sport_id: city_sport.id).uniq
-        render json: level_list.as_json
+        if city_sport
+          slots = "#{city_sport.number_of_players} - (SELECT COUNT(*) FROM user_city_sports WHERE ",
+                  "user_city_sports.level_city_sports_id = level_city_sports.id) as free_slots"
+          attribute = "levels.id as id", "levels.name as name", "levels.description as description", slots[0] + slots[1]
+          level_list = LevelCitySport.select(attribute[0], attribute[1], attribute[2], attribute[3])
+                                     .joins(:level).where(city_sport_id: city_sport.id).uniq
+          if level_list
+            render json: level_list.as_json, status: :ok
+          else
+            render json: {message: "Level doesnot exist"}, status: :not_found
+          end
+        else
+          render json: {message: "City sport doesnot exist"}, status: :not_found
+        end
       end
     end
   end
