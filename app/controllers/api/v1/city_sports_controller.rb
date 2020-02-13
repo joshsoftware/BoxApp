744a5faff691f976ad549161680f3847943c50c9
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-13 14:08:57 +0530

    Handled query exceptions

--- app/controllers/api/v1/city_sports_controller.rb
+++ app/controllers/api/v1/city_sports_controller.rb
@@ -7,12 +7,11 @@ module Api
         city_id = @current_user.city_id
         sport_list = []
         city_sports = CitySport.where(city_id: city_id)
-        if city_sports.nil?
-           render json: {"message" => "Id not found"}, status: 422
-        end 
+        render json: {"error": I18n.t(city_sports.id_not_found)}, status: 422 if city_sports.nil?
         city_sports.find_each do |citysport|
           sport_list += Sport.where(id: citysport.sport_id)
         end
+        render json: {"error": I18n.t(city_sports.sport_list_empty)}, status: 422 if sport_list.empty?
         render json: sport_list.as_json
       end
     end
