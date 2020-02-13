939bfe35f010a0c86638a33f79a4495fe8ab5228
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-02-13 12:45:35 +0530

    Moved exception handling to current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,18 @@ class ApplicationController < ActionController::API
   def validate_token
     user_auth_token = request.headers["user-auth-token"]
     @current_user = current_user(user_auth_token)
-  rescue JWT::DecodeError => e
-    render json: {error: e.message}, status: :unauthorized
-  rescue ActiveRecord::RecordNotFound => e
-    render json: {error: e.message}, status: :unauthorized
   end
 
   def current_user(user_auth_token)
-    token = JsonWebToken.decode(user_auth_token)
-    user_id = token["user_id"]
-    user = User.find(user_id)
+    begin
+      token = JsonWebToken.decode(user_auth_token)
+      user_id = token["user_id"]
+      user = User.find(user_id)
+    rescue JWT::DecodeError => e
+      render json: {error: e.message}, status: :unauthorized
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {error: e.message}, status: :unauthorized
+    end
     user
   end
 end
