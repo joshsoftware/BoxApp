bd38f935f09929c7298b355b8ed9385e8cb40c2d
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-02-19 10:40:18 +0530

    Updated route to all_levels

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post "/city_sport_levels", to: "levels#all_levels"
+      get "/city_sport_levels", to: "levels#all_levels"
       resources :levels
     end
   end
