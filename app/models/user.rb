d635e8374194fe0b8c7f9886f116cc2acfaaafd6
niharikabhoi2020 <niharikabhoi2016@gmail.com>
2020-02-27 17:38:19 +0530

    match scheduler

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,27 @@ class User < ApplicationRecord
     self.first_name = first_name.titleize
     self.last_name = last_name.titleize
   end
+
+
+  def self.getOpponents(current_user_id)
+    user_id = current_user_id
+    all_opponents=[]
+    
+        user_city_sport = UserCitySport.find_by(user_id: user_id)
+        if user_city_sport.nil?
+          return  all_opponents
+        else
+          user_level_city_sports_id = user_city_sport.level_city_sports_id
+          user_city_sport_id = user_city_sport.city_sport_id
+         
+          # fetches all opponents of particular city, sport for a user
+          all_opponents = UserCitySport.where(
+            city_sport_id: user_city_sport_id, level_city_sports_id: user_level_city_sports_id
+          ).where.not(user_id: user_id).joins(:user).select("users.id,users.first_name,users.last_name,users.contact_number")
+         return all_opponents
+       end
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # devise_for :users
   namespace :api do
     namespace :v1 do
-<<<<<<< HEAD
+
       devise_for :users # , :controllers => { registrations: 'registrations' }
       resources :sessions, only: %i[create destroy]
       resource :user, only: %i[show update create]
@@ -16,11 +16,9 @@ Rails.application.routes.draw do
       resources :city_sports
       post "/city_sports/display", to: "city_sports#displaysports"
       # resources :confirmations, only: [:update, :show]
-=======
+
       get "/city_sport_levels", to: "levels#all_levels"
-      resources :levels
-      post "/level_sports", to: "level_sports#create"
->>>>>>> 9cd8e4b91111e90c900f5e4a9c236270d5490b03
+  
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
